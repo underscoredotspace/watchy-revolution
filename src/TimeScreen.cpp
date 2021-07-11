37c6a536a2891ca233fca6ba8f0c00406d8265a5
Charles Haynes <ceh+github@ceh.bz>
2021-07-11 15:25:49 +1000

    simplify time display

--- src/TimeScreen.cpp
+++ src/TimeScreen.cpp
@@ -8,15 +8,5 @@ void TimeScreen::show()
     w->display.setTextColor(GxEPD_WHITE);
     w->display.setFont(&DSEG7_Classic_Bold_53);
     w->display.setCursor(5, 53 + 60);
-    if (w->currentTime.Hour < 10)
-    {
-        w->display.print("0");
-    }
-    w->display.print(w->currentTime.Hour);
-    w->display.print(":");
-    if (w->currentTime.Minute < 10)
-    {
-        w->display.print("0");
-    }
-    w->display.println(w->currentTime.Minute);
+    w->display.printf("%02d:%02d", w->currentTime.Hour, w->currentTime.Minute);
 }
