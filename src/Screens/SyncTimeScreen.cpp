af84716ce8bc7e80de406581855c963630da703a
Charles Haynes <ceh+github@ceh.bz>
2021-07-25 18:37:37 +1000

    clean up timeSync state handling
    Get rid of buf stored in rtc memory

--- src/Screens/SyncTimeScreen.cpp
+++ src/Screens/SyncTimeScreen.cpp
@@ -11,36 +11,61 @@
 #include "time.h"
 
 void timeSyncCallback(struct timeval *tv) {
-  DEBUG("\n*** timeSyncCallback %ld.%ld\n", tv->tv_sec, tv->tv_usec);
+  DEBUG("\n*** timeSyncCallback %ld.%06ld", tv->tv_sec, tv->tv_usec);
 }
 
 RTC_DATA_ATTR const char *SyncTimeScreen::tz = TZ;
 RTC_DATA_ATTR const char *SyncTimeScreen::ntpServer = NTP_SERVER;
-RTC_DATA_ATTR const char *syncMsg = nullptr;
-RTC_DATA_ATTR char buf[200];
+RTC_DATA_ATTR enum SyncState {
+  ready,
+  waitingForSync,
+  wifiFailed,
+  syncFailed,
+  syncSucceeded
+} syncState = ready;
+
+bool showSyncState() {
+  switch (syncState) {
+    case ready:
+      return false;
+    case waitingForSync:
+      Watchy::display.print("\nsynchronizing");
+      break;
+    case wifiFailed:
+      Watchy::display.print("\nwifi connect failed");
+      break;
+    case syncFailed:
+      Watchy::display.print("\nsync failed");
+      break;
+    case syncSucceeded:
+      Watchy::display.print("\nsuccess");
+      break;
+    default:
+      break;
+  }
+  Watchy::display.print("\npress back to exit");
+  return true;
+}
 
 void SyncTimeScreen::show() {
   Watchy::display.setFont(&OptimaLTStd12pt7b);
   Watchy::display.setCursor(0, 0);
-  Watchy::display.print("\n");
-  if (syncMsg) {
-    Watchy::display.print(syncMsg);
+  if (showSyncState()) {
     return;
   }
   time_t tt;
   time(&tt);
   DEBUG("SyncTimeScreen::show time %ld\n", tt);
   sntp_set_time_sync_notification_cb(timeSyncCallback);
-  syncMsg = "synchronizing with\nNTP server";
-  Watchy::display.print(syncMsg);
+  syncState = waitingForSync;
+  showSyncState();
   Watchy::display.display(true);
   Watchy::display.fillScreen(bgColor);
   Watchy::display.setCursor(0, 0);
-  Watchy::display.print("\n");
 
   if (!Watchy::connectWiFi()) {
-    syncMsg = "connect wifi failed\npress back to exit";
-    Watchy::display.print(syncMsg);
+    syncState = wifiFailed;
+    showSyncState();
     return;
   }
   configTzTime(tz, ntpServer);
@@ -54,15 +79,10 @@ void SyncTimeScreen::show() {
       delay(10);
       continue;
     }
-    localtime_r(&now, &t); // uses TZ
-    buf[0] = 0;
-    if (strftime(buf, sizeof(buf),
-                 "time synced\n%A\n%B %d %Y\n%H.%M.%S\npress back to exit",
-                 &t)) {
-      syncMsg = buf;
-    } else {
-      syncMsg = "time synced\npress back to exit";
-    }
+    localtime_r(&now, &t);
+    Watchy::display.print(&t, "\n%A\n%B %d %Y\n%H.%M.%S");
+    syncState = syncSucceeded;
+    showSyncState();
     DEBUG("time synced %04d-%02d-%02d %02d:%02d:%02d dst %d\n",
           t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min,
           t.tm_sec, t.tm_isdst);
@@ -73,14 +93,13 @@ void SyncTimeScreen::show() {
         "SyncTimeScreen::show RTC.write (UTC)  %04d-%02d-%02d "
         "%02d:%02d:%02d\n",
         te.Year + 1970, te.Month, te.Day, te.Hour, te.Minute, te.Second);
-    Watchy::display.print(syncMsg);
     return;
   }
-  syncMsg = "time sync failed\npress back to exit";
-  Watchy::display.print(syncMsg);
+  syncState = syncFailed;
+  showSyncState();
 }
 
-void SyncTimeScreen::back() { syncMsg = nullptr; Screen::back(); }
+void SyncTimeScreen::back() { syncState = ready; Screen::back(); }
 
 // RTC does not know about TZ
 // so DST has to be in app code
