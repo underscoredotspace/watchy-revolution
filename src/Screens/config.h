7ff6afcdc1b6e847b0ae104a00d6e04ace8a2ba6
Charles Haynes <ceh+github@ceh.bz>
2021-07-25 18:36:45 +1000

    change defines to consts

--- src/Screens/config.h
+++ src/Screens/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define TZ "AEST-10AEDT,M10.1.0,M4.1.0/3"
-#define NTP_SERVER "pool.ntp.org"
+constexpr const char *TZ = "AEST-10AEDT,M10.1.0,M4.1.0/3";
+constexpr const char *NTP_SERVER = "pool.ntp.org";
 
 // debugging "#define DEBUGPORT Serial" for debugging output
 #define DEBUGPORT Serial
