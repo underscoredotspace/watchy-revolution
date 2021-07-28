fa21fe4403fcf2d8d7e80b75ab5c10a2cc7af627
Charles Haynes <ceh+github@ceh.bz>
2021-07-28 18:24:36 +1000

    finish moving sntp config into main config.

--- src/config.h
+++ src/config.h
@@ -38,6 +38,7 @@ constexpr const char * WIFI_AP_SSID = "Watchy AP";
 //set time
 const int YEAR_OFFSET = 1970;
 constexpr const char *TZ = "AEST-10AEDT,M10.1.0,M4.1.0/3";
+constexpr const char *NTP_SERVER = "pool.ntp.org";
 
 //BLE OTA
 constexpr const char * BLE_DEVICE_NAME = "Watchy BLE OTA";
