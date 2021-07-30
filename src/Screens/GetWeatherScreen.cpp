3f40543a81ee2a697ecbd6c0c0a29daf1a78442d
Charles Haynes <ceh+github@ceh.bz>
2021-07-30 21:24:49 +1000

    add GetWeatherScreen, working.

--- src/Screens/GetWeatherScreen.cpp
+++ src/Screens/GetWeatherScreen.cpp
@@ -19,19 +19,22 @@ RTC_DATA_ATTR enum GetWeatherState {
   ready,
   waitingForSync,
   wifiFailed,
-  syncFailed,
-  syncSucceeded,
+  httpGetFailed,
+  getWeatherSuccess,
   numGetWeatherStates
 } getWeatherState = ready;
 
 const char *getWeatherMsgs[numGetWeatherStates] = {
-    "\nready", "\nsynchronizing", "\nwifi connect failed", "\nsync failed",
+    "\nready", "\nsynchronizing", "\nwifi connect failed", "\nhttp get failed",
     "\nsuccess"};
 
 weatherData GetWeatherScreen::getWeatherData() {
   // only update if WEATHER_UPDATE_INTERVAL has elapsed i.e. 30 minutes
-  if (millis() - GetWeatherScreen::lastWeatherTS >= WEATHER_UPDATE_INTERVAL) {
-    if (Watchy::connectWiFi()) {  // Use Weather API for live data if WiFi is connected
+  if (now() - lastWeatherTS < WEATHER_UPDATE_INTERVAL) {
+    // too soon to update, just re-use existing values. Not an error
+  } else {
+    if (Watchy::connectWiFi()) {  // Use Weather API for live data if WiFi is
+                                  // connected
       HTTPClient http;
       http.setConnectTimeout(3000);  // 3 second max timeout
       const unsigned int weatherQueryURLSize =
@@ -40,7 +43,7 @@ weatherData GetWeatherScreen::getWeatherData() {
           strlen(OPENWEATHERMAP_APIKEY) + 1;
       char weatherQueryURL[weatherQueryURLSize];
       snprintf(weatherQueryURL, weatherQueryURLSize,
-               "%s?lat=%8.4f&lon=%8.4f&units=%s&appid=%s", OPENWEATHERMAP_URL,
+               "%s?lat=%.4f&lon=%.4f&units=%s&appid=%s", OPENWEATHERMAP_URL,
                SetLocationScreen::lat, SetLocationScreen::lon, TEMP_UNIT,
                OPENWEATHERMAP_APIKEY);
       http.begin(weatherQueryURL);
@@ -48,48 +51,47 @@ weatherData GetWeatherScreen::getWeatherData() {
       if (httpResponseCode == 200) {
         String payload = http.getString();
         JSONVar responseObject = JSON.parse(payload);
-        GetWeatherScreen::currentWeather.temperature = int(responseObject["main"]["temp"]);
-        GetWeatherScreen::currentWeather.weatherConditionCode =
+        currentWeather.temperature = int(responseObject["main"]["temp"]);
+        currentWeather.weatherConditionCode =
             int(responseObject["weather"][0]["id"]);
-        GetWeatherScreen::lastWeatherTS = millis();
+        lastWeatherTS = now();
+        getWeatherState = getWeatherSuccess;
+        Watchy::display.print(getWeatherMsgs[getWeatherState]);
       } else {
-        // http error
+        getWeatherState = httpGetFailed;
+        Watchy::display.print(getWeatherMsgs[getWeatherState]);
       }
       http.end();
       // turn off radios
       WiFi.mode(WIFI_OFF);
       btStop();
     } else {  // No WiFi, use RTC Temperature
+      getWeatherState = wifiFailed;
+      Watchy::display.print(getWeatherMsgs[getWeatherState]);
+
       uint8_t temperature = Watchy::RTC.temperature() / 4;  // celsius
       if (strcmp(TEMP_UNIT, "imperial") == 0) {
         temperature = temperature * 9. / 5. + 32.;  // fahrenheit
       }
-      GetWeatherScreen::currentWeather.temperature = temperature;
-      GetWeatherScreen::currentWeather.weatherConditionCode = 800;
+      currentWeather.temperature = temperature;
+      currentWeather.weatherConditionCode = 800;
     }
   }
-  return GetWeatherScreen::currentWeather;
+  return currentWeather;
 }
 
 void GetWeatherScreen::show() {
+  Watchy::display.fillScreen(bgColor);
   Watchy::display.setFont(&FreeSans12pt7b);
   if (getWeatherState != ready) {
     Watchy::display.print(getWeatherMsgs[getWeatherState]);
   } else {
-    time_t tt;
-    time(&tt);
     getWeatherState = waitingForSync;
     Watchy::display.print(getWeatherMsgs[getWeatherState]);
     Watchy::display.display(true);
     Watchy::display.fillScreen(bgColor);
     Watchy::display.setCursor(0, 0);
-
-    if (!Watchy::connectWiFi()) {
-      getWeatherState = wifiFailed;
-      Watchy::display.print(getWeatherMsgs[getWeatherState]);
-    } else {
-      getWeatherData();
-    }
+    getWeatherData();
   }
   Watchy::display.print("\npress back to exit");
 }
@@ -98,9 +100,3 @@ void GetWeatherScreen::back() {
   getWeatherState = ready;
   Screen::back();
 }
-
-// RTC does not know about TZ
-// so DST has to be in app code
-// so RTC must store UTC
-// RTC computes leap year so you want year to be correct
-// according to how the RTC represents it
--- src/Screens/GetWeatherScreen.h
+++ src/Screens/GetWeatherScreen.h
@@ -6,7 +6,7 @@
 constexpr const float DEFAULT_LOCATION_LATITUDE = 37.8136;
 constexpr const float DEFAULT_LOCATION_LONGDITUDE = -144.9631;
 constexpr const char * OPENWEATHERMAP_APIKEY = "f058fe1cad2afe8e2ddc5d063a64cecb"; //use your own API key :)
-constexpr const char * OPENWEATHERMAP_URL = "http://api.openweathermap.org/data/2.5/weather?";
+constexpr const char * OPENWEATHERMAP_URL = "http://api.openweathermap.org/data/2.5/weather";
 constexpr const char * TEMP_UNIT = "metric"; //use "imperial" for Fahrenheit"
 const int WEATHER_UPDATE_INTERVAL = 30 * 60 * 1000; // 30 minutes in millis
 
