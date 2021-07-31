e08bfdf60cf1eaf526a75bbfffd104d792f32cbb
Charles Haynes <ceh+github@ceh.bz>
2021-07-31 23:22:44 +1000

    refactor synctime into lib, update status display

--- src/Screens/SyncTimeScreen.cpp
+++ src/Screens/SyncTimeScreen.cpp
@@ -1,86 +1,56 @@
 #include "SyncTimeScreen.h"
 
-#include "config.h"  // should be first
-
-#include <Wifi.h>
-
-#include "Adafruit_GFX.h"
 #include "Fonts/FreeSans12pt7b.h"
+#include "SyncTime.h"
 #include "Watchy.h"
-#include "icons.h"
-#include "time.h"
 
-RTC_DATA_ATTR const char *SyncTimeScreen::tz = TZ;
-RTC_DATA_ATTR const char *SyncTimeScreen::ntpServer = NTP_SERVER;
-RTC_DATA_ATTR enum SyncState {
-  ready,
-  waitingForSync,
-  wifiFailed,
-  syncTimeout,
-  syncSucceeded,
-  setFailed,
-  numSyncStates
-} syncState = ready;
+typedef enum { ready, waiting, success, fail, numStates } SyncStates;
+RTC_DATA_ATTR SyncStates syncState = ready;
 
-const char *syncStateMsgs[numSyncStates] = {"\nready", 
-                                            "\nsynchronizing",
-                                            "\nwifi connect failed",
-                                            "\nsync timout", 
-                                            "\nsuccess",
-                                            "\nset time failed"};
+const char* syncStateMsgs[numStates] = {"\nready", "\nwaiting", "\nsuccess",
+                                        "\fail"};
 
-void timeSyncCallback(struct timeval *tv) {
-  setTime(tv->tv_sec); // set system time
-  // consider using tv.tv_usec as well
-  struct tm t;
-  localtime_r(&tv->tv_sec, &t); // assumes tzset and setenv(TZ)
-  Watchy::display.print(&t, "\n%A\n%B %d %Y\n%H.%M.%S");
-  tmElements_t te;
-  breakTime(tv->tv_sec, te);
-  if (Watchy::RTC.write(te) == 0) { // set RTC
-    syncState = syncSucceeded;
-  } else {
-    syncState = setFailed;
+void printTime() {
+  time_t tt = now();
+  tm t;
+  localtime_r(&tt, &t);
+  Watchy::display.print(&t, "\nsuccess\n%I:%M %Z\n%A\n%b %e %Y");
+}
+
+void printSyncState() {
+  if (syncState == success) {
+    printTime();
+    return;
   }
   Watchy::display.print(syncStateMsgs[syncState]);
+  Watchy::display.print("\npress back to exit");
 }
 
 void SyncTimeScreen::show() {
   Watchy::display.fillScreen(bgColor);
   Watchy::display.setFont(&FreeSans12pt7b);
   if (syncState != ready) {
-    Watchy::display.print(syncStateMsgs[syncState]);
-  } else {
-    syncState = waitingForSync;
-    Watchy::display.print(syncStateMsgs[syncState]);
-    Watchy::display.display(true);
-    Watchy::display.fillScreen(bgColor);
-    Watchy::display.setCursor(0, 0);
-
-    if (!Watchy::connectWiFi()) {
-      syncState = wifiFailed;
-      Watchy::display.print(syncStateMsgs[syncState]);
-    } else {
-      sntp_set_time_sync_notification_cb(timeSyncCallback);
-      configTzTime(tz, ntpServer);
-      uint32_t timeout = millis() + 5000;
-      while (millis() < timeout && syncState == waitingForSync) {
-        delay(10);
-      }
-      if (syncState == waitingForSync) {
-        syncState = syncTimeout;
-        Watchy::display.print(syncStateMsgs[syncState]);
-      }
-    }
+    printSyncState();
+    return;
   }
-  Watchy::display.print("\npress back to exit");
+  syncState = waiting;
+  Watchy::display.print(syncStateMsgs[syncState]);
+  Watchy::display.display(true);
+  Watchy::display.fillScreen(bgColor);
+  Watchy::display.setCursor(0, 0);
+
+  if (Watchy_SyncTime::syncTime() == Watchy_SyncTime::success) {
+    syncState = success;
+  } else {
+    syncState = fail;
+  }
+  printSyncState();
 }
 
 void SyncTimeScreen::back() {
   syncState = ready;
   Screen::back();
 }
-
 // RTC does not know about TZ
 // so DST has to be in app code
 // so RTC must store UTC
--- src/Screens/SyncTimeScreen.h
+++ src/Screens/SyncTimeScreen.h
@@ -4,12 +4,6 @@
 
 class SyncTimeScreen : public Screen {
  public:
-  // these should probably live somewhere else (like in Watchy)
-  // but can keep them here for now
-  static const char *tz;
-  static const char *ntpServer;
-
- public:
   SyncTimeScreen() : Screen() {}
   void show() override;
   void back() override;
