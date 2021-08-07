27a67869db2b3647434123842831e7728285a4ab
Charles Haynes <ceh+github@ceh.bz>
2021-08-07 22:38:18 +1000

    add err msgs & comment

--- src/GetWeather.cpp
+++ src/GetWeather.cpp
@@ -19,6 +19,7 @@ weatherData getWeather() {
     return currentWeather;
   }
   if (!Watchy::connectWiFi()) {
+    LOGE("Wifi connect failed");
     // No WiFi, return RTC Temperature (this isn't actually useful...)
     uint8_t temperature = Watchy::RTC.temperature() / 4;  // celsius
     if (strcmp(TEMP_UNIT, "imperial") == 0) {
@@ -42,17 +43,19 @@ weatherData getWeather() {
            "%s?lat=%.4f&lon=%.4f&units=%s&appid=%s", OPENWEATHERMAP_URL,
            loc->lat, loc->lon, TEMP_UNIT,
            OPENWEATHERMAP_APIKEY);
-  http.begin(weatherQueryURL);
+  if (!http.begin(weatherQueryURL)) {
+    LOGE("http.begin failed");
+  }
   int httpResponseCode = http.GET();
   if (httpResponseCode == 200) {
-    String payload = http.getString();  // don't use String
+    String payload = http.getString();
     JSONVar responseObject = JSON.parse(payload);
     currentWeather.temperature = int(responseObject["main"]["temp"]);
     currentWeather.weatherConditionCode =
         int(responseObject["weather"][0]["id"]);
     lastWeatherTS = now();
   } else {
-    LOGI("http response %d", httpResponseCode);
+    LOGE("http response %d", httpResponseCode);
   }
   http.end();
   // turn off radios
@@ -62,3 +65,50 @@ weatherData getWeather() {
 }
 
 }  // namespace Watchy_GetWeather
+
+/* weather response JSON prettified
+{
+    "coord": {
+        "lon": 144.968,
+        "lat": -37.8008
+    },
+    "weather": [
+        {
+            "id": 804,
+            "main": "Clouds",
+            "description": "overcast clouds",
+            "icon": "04n"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 11.76,
+        "feels_like": 11.16,
+        "temp_min": 9.78,
+        "temp_max": 13.05,
+        "pressure": 1026,
+        "humidity": 83
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 0.89,
+        "deg": 228,
+        "gust": 1.79
+    },
+    "clouds": {
+        "all": 90
+    },
+    "dt": 1628326663,
+    "sys": {
+        "type": 2,
+        "id": 2008797,
+        "country": "AU",
+        "sunrise": 1628284471,
+        "sunset": 1628321843
+    },
+    "timezone": 36000,
+    "id": 2171000,
+    "name": "Collingwood",
+    "cod": 200
+}
+*/
