e3de2e20ff1afdea5f9b64b2595f0492abddfb0d
Charles Haynes <ceh+github@ceh.bz>
2021-08-17 11:09:53 +1000

    clean out unused names

--- src/Watchy.h
+++ src/Watchy.h
@@ -21,14 +21,6 @@ void init(String datetime = "");
 void deepSleep();
 float getBatteryVoltage();
 
-void handleButtonPress();
-void showMenu(byte menuIndex, bool partialRefresh);
-void showFastMenu(byte menuIndex);
-void showBattery();
-void showBuzz();
-void showAccelerometer();
-void showUpdateFW();
-void setupWifi();
 bool connectWiFi();
 
 void showWatchFace(bool partialRefresh, Screen *s = screen);
@@ -36,8 +28,6 @@ void setScreen(Screen *s);
 bool pollButtonsAndDispatch();  // returns true if button was pressed
 
 // stored in RTC_DATA_ATTR
-extern int guiState;
-extern int menuIndex;
 extern BMA423 sensor;
 extern bool WIFI_CONFIGURED;
 extern bool BLE_CONFIGURED;
