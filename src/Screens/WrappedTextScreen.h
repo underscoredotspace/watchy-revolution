43d42e2a2a880aeae2d9ae5c739ec0864b45e43a
Charles Haynes <ceh+github@ceh.bz>
2021-08-20 19:29:27 +1000

    add Alt text to WrappedTextScreen
    for testing

--- src/Screens/WrappedTextScreen.h
+++ src/Screens/WrappedTextScreen.h
@@ -5,9 +5,16 @@
 class WrappedTextScreen : public Screen {
  private:
   const char *text;
+  const char *altText;
 
  public:
-  WrappedTextScreen(const char *t, uint16_t bg = GxEPD_WHITE) : Screen(bg), text(t){};
+  WrappedTextScreen(const char *t, const char *at, uint16_t bg = GxEPD_WHITE) : Screen(bg), text(t), altText(at){};
   virtual void show() override;
+  virtual void menu() {  // swap the text
+    const char *t = text;
+    text = altText;
+    altText = t;
+    Watchy::showWatchFace(true, this);
+  };
   void setText(const char *t) { text = t; };
 };
