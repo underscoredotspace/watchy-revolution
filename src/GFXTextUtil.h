9403314a47e53565e36172c37a75d9622657c51f
Charles Haynes <ceh+github@ceh.bz>
2021-08-20 21:25:28 +1000

    update doc string in .h file

--- src/GFXTextUtil.h
+++ src/GFXTextUtil.h
@@ -3,17 +3,18 @@
 #include <Adafruit_GFX.h>
 
 /**
- * @brief draws a text string inside a rectangular area of an Adafruit GFX. Wraps the text on
- *        word boundaries (any non-alphanumeric.) Fits as much text in the area as possible.
- *        uses the current font and font color.
- * 
- * @param g an Adafruit GFX object
- * @param x upper left corner of the viewport
- * @param y upper left corner of the viewport
- * @param w width of the viewport
- * @param h height of the viewport
- * @param t text to draw
- * @param f the font to draw the text
+ * @brief draw word wrapped text within a bounding box. Takes GFX, bounding box,
+ *        text string, and font. Handles embedded LF, words that won't fit on one
+ *        line (hard wraps), text that won't all fit (fits as much as possible),
+ *        excess whitespace
+ *
+ * @param g Adafruit_GFX to draw on
+ * @param x upper left of bounding box (left)
+ * @param y upper left of bounding box (top)
+ * @param w width of bounding box
+ * @param h height of bounding box
+ * @param t text to draw. will draw as much as fits
+ * @param f font to use
  */
 void drawWordWrappedText(Adafruit_GFX &g, int16_t x, int16_t y, int16_t w,
                          int16_t h, const char *t, const GFXfont *f);
